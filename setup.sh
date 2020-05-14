143d810b66745d8440bb75c55f8db6cf3ed4620c
sabinevidal <svidal@live.co.za>
2020-05-14 12:56:40 +0200

    Initial Heroku Commit

--- setup.sh
+++ setup.sh
@@ -1,1 +1,6 @@
 
+export DATABASE_URL=postgresql://postgres:postgres@localhost:5432/YOUR_DATABASE
+export AUTH0_DOMAIN=text-tutor.eu.auth0.com
+export ALGORITHMS=['RS256']
+export API_AUDIENCE=YOUR_AUDIENCE
+export TEST_AUTH=
