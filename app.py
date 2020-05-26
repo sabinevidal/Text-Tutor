9bc8b24fc4ba3d23ccb9909bffe80dbbc5823c6d
sabinevidal <svidal@live.co.za>
2020-05-26 23:01:04 +0200

    feat: added error handling

--- app.py
+++ app.py
@@ -134,7 +134,46 @@ def create_app(test_config=None):
             'subject': new_subject.format()
         })
 
+# -----------------------------------------------------------
+# Error Handlers
+# -----------------------------------------------------------
+
+    @app.errorhandler(400)
+    def bad_request_error(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "bad request"
+        }), 400
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "resource not found"
+        }), 404
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return jsonify({
+            "success": False,
+            "error": 500,
+            "message": "internal server error"
+        }), 422
+
     return app
+
+
+
 
 app = create_app()
 
