490f93f109e0d57922daa127b7970948146fa3c8
sabinevidal <svidal@live.co.za>
2020-06-01 02:58:54 +0200

    Fix: fixes for connection refused

--- backend/setup.sh
+++ backend/setup.sh
@@ -3,3 +3,5 @@ export DATABASE_URL=postgresql://sabinevidal:password@localhost:5432/text-tutor
 export AUTH0_DOMAIN=text-tutor.eu.auth0.com
 export ALGORITHMS=['RS256']
 export API_AUDIENCE=https://text-tutor/
+export FLASK_APP=app.py
+export FLASK_ENV=development
